e193e4e3b0e9b3ad55e5713cc85cc70a81647089
ewanklomp <ewanklomp@hotmail.com>
2016-11-16 16:16:42 +0100

    Test om build succes te chekcen

--- R_naar_P.py
+++ R_naar_P.py
@@ -16,7 +16,7 @@ def rnaarp():
     if tcrit > resultaat:
         print ("Het is niet statistically significant aangezien het lager is dan tcrit = " + str(tcrit) + " (df=" + str(df) + ")")
         print ("Schrijf het op als: r= " + str(r) + ",n= " + str(samplesize) + ", p >.05, two-tails")
-    else:
+    else random text om te kijken of hij failt of niet: 
         print ("Het is statistically significant aangezien het hoger is dan tcrit = " + str(tcrit) + " (df=" + str(df) + ")")
         print ("Schrijf het op als: r= " + str(r) + ",n= " + str(samplesize) + ", p <.05, two-tails") 
     doorgaan()
